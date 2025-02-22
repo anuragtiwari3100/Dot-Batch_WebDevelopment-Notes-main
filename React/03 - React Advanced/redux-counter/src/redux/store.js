4f947976f2da22e5333dd1e3ef5dac95fda68eec
Anurag Tiwari <anuragtiwarigpj2005@gmail.com>
2025-02-22 11:37:19 +0530

    Update store.js

--- React/03 - React Advanced/redux-counter/src/redux/store.js
+++ React/03 - React Advanced/redux-counter/src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import CounterSlice from './slices/CounterSlice'
 
 export const store = configureStore({
-  reducer: {
-    counter: CounterSlice
+    reducer: {                // <- Ye object hai jo batata hai kaunsa reducer kis state ko handle karega
+    counter: CounterSlice    // // "counter" state ko counterReducer manage karega
   },
 })
